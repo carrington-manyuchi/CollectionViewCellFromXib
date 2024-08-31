7df969d9006247b07b508fc207641e32b6288aef
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-31 11:10:02 +0200

    Initial Commit

---
+++ CollectionViewCellFromXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewCellFromXib
+//
+//  Created by Manyuchi, Carrington C on 2024/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
